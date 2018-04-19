f250606458c79e263b97600dfa46efe12a88ef5d
Dan Berry <dberry@apple.com>
2018-04-19 15:53:48 -0600

    Initial Commit

---
+++ Squares/SquaresView.h
@@ -0,0 +1,13 @@
+//
+//  SquaresView.h
+//  Squares
+//
+//  Created by Daniel Berry on 4/19/18.
+//  Copyright © 2018 danberry. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface SquaresView : ScreenSaverView
+
+@end
